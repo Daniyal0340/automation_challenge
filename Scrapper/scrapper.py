6aecca4ee73a8674fef6f369f60f39bc847462f2
Daniyal Mazher <daniyal.mazher0340@gmail.com>
2024-04-27 03:16:50 +0500

    moved locators

--- Scrapper/scrapper.py
+++ Scrapper/scrapper.py
@@ -26,6 +26,8 @@ logger.addHandler(handler)
 
 class LaTimes:
     def __init__(self, search, topics, month_range):
+        if month_range == 0:
+            month_range = 1
         self.browser_lib = Selenium()
         self.work_item_lib = WorkItems()
         self.excel_lib = Files()
@@ -60,7 +62,7 @@ class LaTimes:
             time.sleep(2)
             if self.browser_lib.does_page_contain_element(f'//span[text()="{topic}"]'):
                 logger.info(f'Selecting topic {topic}')
-                for see_all_button in self.browser_lib.find_elements('//span[text()="See All"]'):
+                for see_all_button in self.browser_lib.find_elements(self.locator.see_all_btn):
                     self.browser_lib.scroll_element_into_view(see_all_button)
                     see_all_button.click()
                 self.browser_lib.scroll_element_into_view(f'//span[text()="{topic}"]')
@@ -69,29 +71,29 @@ class LaTimes:
     def read_news(self):
         time.sleep(2)
         self.browser_lib.execute_javascript('window.scrollTo(0, document.body.scrollHeight);')
-        self.browser_lib.wait_until_page_contains_element('//div[@class="search-results-module-page-counts"]')
-        pages = self.browser_lib.find_element('//div[@class="search-results-module-page-counts"]').text.split('of')[-1].strip()
+        self.browser_lib.wait_until_page_contains_element(self.locator.page_count)
+        pages = self.browser_lib.find_element(self.locator.page_count).text.split('of')[-1].strip()
         for page_number in range(int(pages.replace(',', ''))):
             logger.info(f'reading news at page {page_number}')
             while True:
-                news = self.browser_lib.find_elements('//ul[@class="search-results-module-results-menu"]/li')
-                images = self.browser_lib.find_elements('//div[@class="promo-media"]')
+                news = self.browser_lib.find_elements(self.locator.news_list)
+                images = self.browser_lib.find_elements(self.locator.image)
                 if len(news) != len(images):
                     self.browser_lib.reload_page()
                     time.sleep(2)
                 else:
                     break
-            for news_element in self.browser_lib.find_elements('//ul[@class="search-results-module-results-menu"]/li'):
-                date = news_element.find_element(By.XPATH, './/p[@class="promo-timestamp"]').text
+            for news_element in self.browser_lib.find_elements(self.locator.news_list):
+                date = news_element.find_element(By.XPATH, self.locator.date).text
                 try:
                     datetime_obj = parse(date)
                     if datetime_obj < self.month_range:
                         return None
                 except ParserError:
                     ...
-                title = news_element.find_element(By.XPATH, './/h3[@class="promo-title"]').text
-                desc = news_element.find_element(By.XPATH, './/p[@class="promo-description"]').text
-                image = news_element.find_element(By.XPATH, './/img')
+                title = news_element.find_element(By.XPATH, self.locator.title).text
+                desc = news_element.find_element(By.XPATH, self.locator.description).text
+                image = news_element.find_element(By.XPATH, self.locator.image)
                 image_url = image.get_attribute('src')
                 response = requests.get(image_url)
                 image_path = os.path.join(f"{os.getcwd()}/output/{slugify(title, separator='_')}.png")
@@ -108,7 +110,7 @@ class LaTimes:
                 self.data['Image Path'].append(image_path)
                 self.data['Price Status'].append(str(contain_amount))
                 self.data['Phrase Count'].append((title + desc).count(self.search_phrase))
-            self.browser_lib.click_element_when_visible('//div[@class="search-results-module-next-page"]')
+            self.browser_lib.click_element_when_visible(self.locator.next_page)
 
     def save_news(self):
         self.excel_lib.create_workbook(path=f"{os.getcwd()}/output/news.xlsx", fmt="xlsx")
@@ -124,9 +126,9 @@ class LaTimes:
             logger.info('Applying topic filter')
             self.browser_lib.execute_javascript('window.scrollTo(0, document.body.scrollHeight);')
             time.sleep(2)
-            if self.browser_lib.does_page_contain_element('//modality-custom-element'):
-                self.browser_lib.wait_until_page_contains_element('//modality-custom-element')
-                pop_up = self.browser_lib.find_element('//modality-custom-element')
+            if self.browser_lib.does_page_contain_element(self.locator.pop_up_module):
+                self.browser_lib.wait_until_page_contains_element(self.locator.pop_up_module)
+                pop_up = self.browser_lib.find_element(self.locator.pop_up_module)
                 pop_close_button = self.browser_lib.driver.execute_script('return arguments[0].shadowRoot.querySelector("a")', pop_up)
                 pop_close_button.click()
             self.select_topic()
